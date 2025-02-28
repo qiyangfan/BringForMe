19db6c0158779894c6820e6ab509565df2c2c4e2
yangfan qi <2979414Q@student.gla.ac.uk>
2025-02-28 12:31:25 +0000

    feat: Extend phone number length and add validators for country code and phone number
    Signed-off-by: yangfan qi <2979414Q@student.gla.ac.uk>

--- user/models.py
+++ user/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
-    country_code = models.CharField(max_length=3)
-    phone = models.CharField(max_length=12)
+    country_code = models.CharField(max_length=4)
+    phone = models.CharField(max_length=15)
     password = models.CharField(max_length=255)
     nickname = models.CharField(max_length=20)
     avatar = models.ImageField(null=True, blank=True)
--- user/validators.py
+++ user/validators.py
@@ -1,6 +1,18 @@
 from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class PasswordValidator(RegexValidator):
-    regex = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$'
-    message = 'Your password must be 8-20 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%%^&*).'
+    regex = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~])[A-Za-z\d!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~]{8,20}$'
+    message = _(
+        'Your password must be 8 to 20 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character from the following set: !"#$%%&\'()*+,-./:;<=>?@[\]^_{|}~`.')
+
+
+class CountryCodeValidator(RegexValidator):
+    regex = r'^\+?\d{1,3}$'
+    message = _('Invalid country code format. Please enter a valid country code (e.g., +1, +86).')
+
+
+class PhoneValidator(RegexValidator):
+    regex = r'^\d{1,15}$'
+    message = _('Invalid phone number format. Please enter a valid phone number with digits only.')
